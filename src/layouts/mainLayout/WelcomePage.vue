1db52606cdd226da840545de55b14163716074b2
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-07-24 16:50:02 -0400

    Se agrega molde de pagina de bienvenida

---
+++ src/layouts/mainLayout/WelcomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome-page">
+      <h1>Welcome to our page</h1>
+  </div>
+</template>
+
+<script>
+import '../../assets/styles/layouts/main/main.css'
+export default {
+    name: "WelcomePage"
+}
+</script>
+
+<style>
+
+</style>
