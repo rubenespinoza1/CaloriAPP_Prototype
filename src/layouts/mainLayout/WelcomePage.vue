8795239c4e89ad9b5fdcded39344e4a030b397e6
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-07-25 18:23:16 -0400

    Cambios para ver menu sidebar

--- src/layouts/mainLayout/WelcomePage.vue
+++ src/layouts/mainLayout/WelcomePage.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="welcome-page">
-   
+  <h1>
+     Bienvenido a Caloriapp
+  </h1>
+  <div>
+    <p>
+      En esta pagina web pordras
+    </p>
+    <ul>
+      <li>
+        Regular tu peso por medio de las calorias que consumes
+      </li>
+      <li>
+        dasdasd
+      </li>
+      <li>
+        adsdasdfsdfs
+      </li>
+    </ul>
   </div>
+  </div>
+
 </template>
 
 <script>
