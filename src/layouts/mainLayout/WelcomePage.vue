c325c07446f3ad87f5f8d2408726953c36bbdda1
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-07-25 17:26:21 -0400

    Se intenta corregir error sidebar

--- src/layouts/mainLayout/WelcomePage.vue
+++ src/layouts/mainLayout/WelcomePage.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="welcome-page">
-      
-      <div class="welcome-message big">
-          <h1>Bienvenido a CaloriAPP</h1>
-      </div>
-      <div class="welcome-message small">
-          <p>
-              La aplicacion que te permitira controlar tu peso rapidamente
-          </p>
-      </div>
+   
   </div>
 </template>
 
 <script>
-import '../../assets/styles/layouts/main/main.css'
+import "../../assets/styles/layouts/main/main.css";
 export default {
-    name: "WelcomePage"
-}
+  name: "WelcomePage",
+};
 </script>
 
 <style>
-
 </style>
